b3005e4fe24b57d9d9284ae30c359fa2d23105c0
S460 <aleksandar.ta6ev@gmail.com>
2021-04-19 19:03:56 +0300

    Added validate coords function

--- spec/Gameboard.spec.ts
+++ spec/Gameboard.spec.ts
@@ -5,14 +5,16 @@ describe('Gameboard class', () => {
 	test('Place small ship', () => {
 		const gameboard = new Gameboard();
 		const ship = new Ship(1);
-		gameboard.placeShip(ship, [1, 1, 'v']);
-		expect(gameboard.board[1][1]).toBe(0);
+		gameboard.placeShip(ship, [0, 0, 'v']);
+		expect(gameboard.board[0][0]).toBe(0);
 		expect(gameboard.board[0][1]).toBeFalsy();
 	});
 
 	test('Pass impossible coords', () => {
 		const gameboard = new Gameboard();
 		const ship = new Ship(2);
-		expect(gameboard.validateCoords(ship, [10, 10, 'v'])).toBe(false);
+		expect(gameboard.validateCoords(ship, [9, 9, 'v'])).toBe(false);
+		expect(gameboard.validateCoords(ship, [9, 2, 'h'])).toBe(true);
+		expect(gameboard.validateCoords(ship, [10, 2, 'h'])).toBe(false);
 	});
 });
--- src/Gameboard.ts
+++ src/Gameboard.ts
@@ -18,13 +18,10 @@ class Gameboard {
 	];
 
 	public validateCoords(ship: ShipInterface, coords: coordsType): boolean {
+		if (coords[0] > 9 || coords[1] > 9) return false;
 		for (let i = 0; i < ship.length; i++) {
-			if (coords[2] === 'v') {
-				if (coords[0] + i > this.board.length) return false;
-			}
-			if (coords[2] === 'h') {
-				if (coords[1] + i > this.board.length) return false;
-			}
+			const max = coords[2] === 'v' ? coords[0] + i : coords[1] + i;
+			if (max + 1 > this.board.length) return false;
 		}
 
 		return true;
@@ -32,7 +29,7 @@ class Gameboard {
 
 	public placeShip(ship: ShipInterface, coords: coordsType): void {
 		this.ships.push(ship);
-		this.board[coords[0]][coords[1]] = ship.length - 1;
+		this.board[coords[0]][coords[1]] = this.ships.length - 1;
 	}
 }
 
