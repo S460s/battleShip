097cb39a7fe0d199532a0d2ad23e8304210a25aa
S460 <aleksandar.ta6ev@gmail.com>
2021-04-19 22:08:09 +0300

    Made coords and object instead of a tuple

--- spec/Gameboard.spec.ts
+++ spec/Gameboard.spec.ts
@@ -5,33 +5,41 @@ describe('Gameboard class', () => {
 	test('Place small ship', () => {
 		const gameboard = new Gameboard();
 		const ship = new Ship(1);
-		gameboard.placeShip(ship, [0, 0, 'v']);
+		gameboard.placeShip(ship, { x: 0, y: 0, flag: 'v' });
 		expect(gameboard.board[0][0]).toBe(0);
 		expect(gameboard.board[0][1]).toBeFalsy();
 	});
 
 	test('Pass impossible coords', () => {
 		const gameboard = new Gameboard();
-		expect(gameboard.placeShip(new Ship(3), [4, 11, 'h'])).toBe(false);
-		expect(gameboard.placeShip(new Ship(3), [4, 7, 'h'])).toBe(false);
-		expect(gameboard.placeShip(new Ship(3), [1, 7, 'v'])).toBe(true);
+		expect(gameboard.placeShip(new Ship(3), { y: 4, x: 11, flag: 'h' })).toBe(
+			false
+		);
+		expect(gameboard.placeShip(new Ship(3), { y: 4, x: 7, flag: 'h' })).toBe(
+			false
+		);
+		expect(gameboard.placeShip(new Ship(3), { y: 1, x: 7, flag: 'v' })).toBe(
+			true
+		);
 	});
 
 	test('Place longer ship', () => {
 		const gameboard = new Gameboard();
-		expect(gameboard.placeShip(new Ship(2), [5, 4, 'h'])).toBe(true);
+		expect(gameboard.placeShip(new Ship(2), { y: 5, x: 4, flag: 'v' })).toBe(
+			true
+		);
 	});
 
 	test('Check for placed ships', () => {
 		const gameboard = new Gameboard();
 		const ship = new Ship(3);
 
-		expect(gameboard.placeShip(ship, [5, 5, 'h'])).toBe(true);
+		expect(gameboard.placeShip(ship, { y: 5, x: 5, flag: 'h' })).toBe(true);
 		expect(gameboard.board[5][5]).toBe(0);
 		expect(gameboard.board[5][6]).toBe(0);
 		expect(gameboard.board[5][6]).toBe(0);
 
-		gameboard.placeShip(new Ship(1), [2, 2, 'h']);
+		gameboard.placeShip(new Ship(1), { y: 2, x: 2, flag: 'h' });
 		expect(gameboard.board[2][2]).toBe(1);
 	});
 });
--- src/Gameboard.ts
+++ src/Gameboard.ts
@@ -1,8 +1,12 @@
 import { ShipInterface } from './Ship';
-type coordsType = [number, number, 'h' | 'v'];
-
+interface coordsInterface {
+	y: number;
+	x: number;
+	flag: 'h' | 'v';
+}
 class Gameboard {
 	ships: ShipInterface[] = [];
+	attackedCoords = [];
 
 	board: number[][] | string[][] = [
 		['', '', '', '', '', '', '', '', '', ''],
@@ -17,35 +21,37 @@ class Gameboard {
 		['', '', '', '', '', '', '', '', '', ''],
 	];
 
-	private validateCoords(ship: ShipInterface, coords: coordsType): boolean {
+	private validateCoords(
+		ship: ShipInterface,
+		coords: coordsInterface
+	): boolean {
 		// Min coord is 0 and max is 9
-		if (coords[0] > 9 || coords[1] > 9) return false;
+		if (coords.y > 9 || coords.x > 9) return false;
 
-		const max = coords[2] === 'v' ? coords[0] : coords[1];
+		const max = coords.flag === 'v' ? coords.y : coords.x;
 		if (max + ship.length + 1 > this.board.length) return false;
 
 		for (let i = 0; i < ship.length; i++) {
 			//todo => refactor
 			const [x, y] =
-				coords[2] === 'v'
-					? [coords[0] + i, coords[1]]
-					: [coords[0], coords[1] + i];
+				coords.flag === 'v'
+					? [coords.y + i, coords.x]
+					: [coords.y, coords.x + i];
 
 			if (this.board[x][y] !== '') false;
 		}
 		return true;
 	}
 
-	public placeShip(ship: ShipInterface, coords: coordsType): boolean {
+	public placeShip(ship: ShipInterface, coords: coordsInterface): boolean {
 		if (!this.validateCoords(ship, coords)) return false;
-
 		this.ships.push(ship);
 		for (let i = 0; i < ship.length; i++) {
 			//todo => refactor
 			const [x, y] =
-				coords[2] === 'v'
-					? [coords[0] + i, coords[1]]
-					: [coords[0], coords[1] + i];
+				coords.flag === 'v'
+					? [coords.y + i, coords.x]
+					: [coords.y, coords.x + i];
 
 			this.board[x][y] = this.ships.length - 1;
 		}
